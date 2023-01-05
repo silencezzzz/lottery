65921f30a7c4a6eff82c6a3dcc6637ade3c0f81b
abelchen <abelchen@xiaoe-tech.com>
2023-01-05 18:17:39 +0800

    first commit

---
+++ js/member.js
@@ -0,0 +1,147 @@
+var member =
+    [
+      {
+        "phone": "",
+        "name": "wesleyli(李威)"
+      },
+      {
+        "phone": "",
+        "name": "abelchen(陈洪升)"
+      },
+      {
+        "phone": "",
+        "name": "amyliu(刘军辉)"
+      },
+      {
+        "phone": "",
+        "name": "auraou(欧静然)"
+      },
+      {
+        "phone": "",
+        "name": "barretai(艾兵)"
+      },
+      {
+        "phone": "",
+        "name": "chschen(陈泽仕)"
+      },
+      {
+        "phone": "",
+        "name": "corinchen(陈宇航)"
+      },
+      {
+        "phone": "",
+        "name": "echozhang(张子洁)"
+      },
+      {
+        "phone": "",
+        "name": "emilyxia(夏丹丹)"
+      },
+      {
+        "phone": "",
+        "name": "fijizuo(左飞杰)"
+      },
+      {
+        "phone": "",
+        "name": "graypan(潘博)"
+      },
+      {
+        "phone": "",
+        "name": "haideeye(叶文杰)"
+      },
+      {
+        "phone": "",
+        "name": "helayelzhao(赵丽娜)"
+      },
+      {
+        "phone": "",
+        "name": "henrywang(王晟源)"
+      },
+      {
+        "phone": "",
+        "name": "irisyang(杨晓霞)"
+      },
+      {
+        "phone": "",
+        "name": "jamiejiang(江玫莹)"
+      },
+      {
+        "phone": "",
+        "name": "jerryyang(杨锋涛)"
+      },
+      {
+        "phone": "",
+        "name": "jinzhuli(李金珠)"
+      },
+      {
+        "phone": "",
+        "name": "juniorzhu(朱辉)"
+      },
+      {
+        "phone": "",
+        "name": "karlieLiao(廖晓怡)"
+      },
+      {
+        "phone": "",
+        "name": "kekejiang(蒋科)"
+      },
+      {
+        "phone": "",
+        "name": "mingxingzhang(张明星)"
+      },
+      {
+        "phone": "",
+        "name": "mittacychen(陈铭涛)"
+      },
+      {
+        "phone": "",
+        "name": "nicholasfeng(酆益)"
+      },
+      {
+        "phone": "",
+        "name": "rickypeng(彭凯)"
+      },
+      {
+        "phone": "",
+        "name": "richardfeng(冯江尧)"
+      },
+      {
+        "phone": "",
+        "name": "roseding(丁杰)"
+      },
+      {
+        "phone": "",
+        "name": "sasumihuang(黄仕荣)"
+      },
+      {
+        "phone": "",
+        "name": "vhomelu(卢伟鸿)"
+      },
+      {
+        "phone": "",
+        "name": "vincezhang(张远智)"
+      },
+      {
+        "phone": "",
+        "name": "viviren(任婷婷)"
+      },
+      {
+        "phone": "",
+        "name": "websterli(李俊杰)"
+      },
+      {
+        "phone": "",
+        "name": "wenqiangfeng(冯文强)"
+      },
+      {
+        "phone": "",
+        "name": "yumiguo(郭渝)"
+      },
+      {
+        "phone": "",
+        "name": "zoeytan(谭梓琪)"
+      },
+      {
+        "phone": "",
+        "name": "zorohuang(黄志伟)"
+      }
+    ]
